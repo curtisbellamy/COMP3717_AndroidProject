5a76de9c171b27141e3cc9ba08b5f23cfa7781e5
curtisbellamy <35937828+curtisbellamy@users.noreply.github.com>
2019-02-10 16:52:00 -0800

    Added CulturalEvent.

--- app/src/main/java/ca/bcit/comp3717_androidproject/MainActivity.java
+++ app/src/main/java/ca/bcit/comp3717_androidproject/MainActivity.java
@@ -1,13 +1,28 @@
 package ca.bcit.comp3717_androidproject;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = MainActivity.class.getSimpleName();
+    private ProgressDialog pDialog;
+    private ListView lv;
+
+    private ArrayList<CulturalEvent> toonList;
+
+    // URL to get contacts JSON
+    private static String SERVICE_URL = "http://opendata.newwestcity.ca/downloads/cultural-events/EVENTS.json";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView)findViewById(R.id.listView);
     }
 }
